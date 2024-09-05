8db6896f2fa1f22c10cdc2155ef72f9dc567c76d
David Pedroso <dservsolutions@gmail.com>
2024-09-04 20:43:40 -0400

    Working on Suggest Page

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import random
-from flask import render_template, Flask
+from crypt import methods
+from flask import render_template, Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean, result_tuple
 
 app = Flask(__name__)
 
+
 class Base(DeclarativeBase):
     pass
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+
 # CAFE Table Configuration
 class Cafe(db.Model):
-    id:Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     map_url: Mapped[str] = mapped_column(String(500), nullable=False)
     img_url: Mapped[str] = mapped_column(String(500), nullable=False)
@@ -26,6 +30,7 @@ class Cafe(db.Model):
     has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
+
 
 with app.app_context():
     db.create_all()
@@ -38,20 +43,36 @@ def home():
     random_cafe = random.choice(result)
     return render_template('index.html', cafe=random_cafe)
 
+
 @app.route('/places')
 def places_registered():
     query = db.session.execute(db.select(Cafe.location))
     result = query.scalars().all()
     return render_template('places_registered.html', places=result)
 
-@app.route('/suggest_a_place')
+
+@app.route('/suggest_a_place', methods=["POST", "GET"])
 def suggest():
+    if request.method == "POST":
+        data = request.form
+        print(data)
+        # print(f"Name: {data['name']}, Map URL: {data['url']}, Img URL: {data['img_url']},"
+        #       f" Location: {data['location']}, Sockets: {data['sockets']}")
+    else:
+        print("Error")
+    
     return render_template('suggest_a_place.html')
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["POST", "GET"])
 def contact():
+    if request.method == "POST":
+        data = request.form
+        print(f"{data['name']}, {data['email']}")
+    else:
+        print("Error")
     return render_template('contact_me.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
