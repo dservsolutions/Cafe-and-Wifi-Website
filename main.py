fc97784f1731739615a281cf1f7a6a0e7203b022
David Pedroso <ppdavisito89@gmail.com>
2024-09-25 17:01:03 -0400

    Making delete and edit routes.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from crypt import methods
+from xml.sax.handler import property_interning_dict
+
 from flask import render_template, Flask, request, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm.sync import update
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
@@ -9,11 +14,12 @@ app = Flask(__name__)
 class Base(DeclarativeBase):
     pass
 
+
 app.config['SECRET_KEY'] = "ZJ\x9e\xc74N\x8d\xe5\xe8\x05\xd2w\xab\xbe\\\xe2+\x01\xac\x9c\x94\xa7\xfbc"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-                                                
+
 
 # CAFE Table Configuration
 class Cafe(db.Model):
@@ -29,31 +35,28 @@ class Cafe(db.Model):
     has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
 
+    #
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 
 with app.app_context():
     db.create_all()
 
+
 def index_card():
     query = db.session.execute(db.select(Cafe))
     result = query.scalars().all()
-    items = [ data for data in result]
-    return items
+    items = [data for data in result]
+    return items, result
+
 
 @app.route('/')
 def home():
     items = index_card()
     item_1 = items[0]
     item_2 = items[1]
-    return render_template('index.html', first_card = item_1, second_card = item_2)
-
-
-@app.route('/all_places')
-def view_all():
-    query = db.session.execute(db.select(Cafe))
-    result = query.scalars().all()
-    return render_template('all_places.html', places=result )
-
+    return render_template('index.html', first_card=item_1, second_card=item_2)
 
 
 @app.route('/add', methods=["POST", "GET"])
@@ -71,16 +74,43 @@ def add():
             location=data.get('location'),
             seats=data.get('seats'),
             coffee_price=data.get('coffee_price'),
-            has_toilet = toilet,
-            has_wifi = wifi,
-            has_sockets = sockets,
-            can_take_calls = calls
-            )
+            has_toilet=toilet,
+            has_wifi=wifi,
+            has_sockets=sockets,
+            can_take_calls=calls
+        )
         db.session.add(new_cafe)
         db.session.commit()
         flash("Save Success")
         return redirect(url_for('home'))
     return render_template('add.html')
+
+
+@app.route('/all_places')
+def all_places():
+    query = db.session.execute(db.select(Cafe))
+    result = query.scalars().all()
+    return render_template('all_places.html', places=result)
+
+@app.route('/edit', methods=["POST", "GET"])
+def edit():
+    if request.method == "POST":
+        # Update Record
+        cafe_id = request.args.get('id')
+        to_update = db.get_or_404(Cafe, cafe_id)
+        db.session.commit()
+        return redirect(url_for('all_places'))
+    cafe_id = request.args.get('id')
+    cafe_selected = db.get_or_404(Cafe, cafe_id)
+    return render_template('edit.html' ,cafe=cafe_selected)
+
+@app.route('/delete')
+def delete():
+    cafe_id = request.args.get('id')
+    print(cafe_id)
+    return redirect(url_for('all_places'))
+
+
 
 
 if __name__ == "__main__":
